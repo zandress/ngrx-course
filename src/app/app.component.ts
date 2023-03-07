2b4142a65f63af50d33975688a7ba7e7e5dd5c2e
Zach <74432623+zandress@users.noreply.github.com>
2023-03-07 11:04:40 -0500

    course work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,61 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from "@angular/router";
+import { AppState } from "./reducers";
+import { isLoggedIn, isLoggedOut } from "./auth/auth.selectors";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  loading = true;
 
-    loading = true;
+  isLoggedIn$: Observable<boolean>;
 
-    constructor(private router: Router) {
+  isLoggedOut$: Observable<boolean>;
 
-    }
+  constructor(private router: Router, private store: Store<AppState>) {}
 
-    ngOnInit() {
-
-      this.router.events.subscribe(event  => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
-            break;
-          }
-
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
         }
-      });
 
-    }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
 
-    logout() {
+    this.isLoggedIn$ = this.store
+      .pipe(
+        select(isLoggedIn)
+      );
 
-    }
+    this.isLoggedOut$ = this.store
+      .pipe(
+        select(isLoggedOut)
+      );
+  }
 
+  logout() {}
 }
---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,21 @@
+import { ActionReducerMap, createReducer, on } from "@ngrx/store";
+import { AuthActions } from "../action-types";
+import { User } from "../model/user.model";
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined,
+};
+
+export const authReducer = createReducer(
+  initialAuthState,
+
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  })
+);
