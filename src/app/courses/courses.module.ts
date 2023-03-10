1b57bc176c3fa2107cf3abc209a4386f299a5b68
Zach <74432623+zandress@users.noreply.github.com>
2023-03-10 15:11:07 -0500

    setting up ngrx data

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,43 +1,46 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HomeComponent} from './home/home.component';
-import {CoursesCardListComponent} from './courses-card-list/courses-card-list.component';
-import {EditCourseDialogComponent} from './edit-course-dialog/edit-course-dialog.component';
-import {CoursesHttpService} from './services/courses-http.service';
-import {CourseComponent} from './course/course.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {RouterModule, Routes} from '@angular/router';
-import { EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
-import {compareCourses, Course} from './model/course';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home/home.component";
+import { CoursesCardListComponent } from "./courses-card-list/courses-card-list.component";
+import { EditCourseDialogComponent } from "./edit-course-dialog/edit-course-dialog.component";
+import { CoursesHttpService } from "./services/courses-http.service";
+import { CourseComponent } from "./course/course.component";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterModule, Routes } from "@angular/router";
+import { EntityDefinitionService, EntityMetadataMap } from "@ngrx/data";
+import { compareCourses, Course } from "./model/course";
 
-import {compareLessons, Lesson} from './model/lesson';
-
+import { compareLessons, Lesson } from "./model/lesson";
+import { CourseEntityService } from "./services/course-entity.service";
 
 export const coursesRoutes: Routes = [
   {
-    path: '',
-    component: HomeComponent
-
+    path: "",
+    component: HomeComponent,
   },
   {
-    path: ':courseUrl',
-    component: CourseComponent
-  }
+    path: ":courseUrl",
+    component: CourseComponent,
+  },
 ];
+
+const entityMetaData: EntityMetadataMap = {
+  Course: {},
+};
 
 @NgModule({
   imports: [
@@ -57,30 +60,25 @@ export const coursesRoutes: Routes = [
     MatDatepickerModule,
     MatMomentDateModule,
     ReactiveFormsModule,
-    RouterModule.forChild(coursesRoutes)
+    RouterModule.forChild(coursesRoutes),
   ],
   declarations: [
     HomeComponent,
     CoursesCardListComponent,
     EditCourseDialogComponent,
-    CourseComponent
+    CourseComponent,
   ],
   exports: [
     HomeComponent,
     CoursesCardListComponent,
     EditCourseDialogComponent,
-    CourseComponent
+    CourseComponent,
   ],
   entryComponents: [EditCourseDialogComponent],
-  providers: [
-    CoursesHttpService
-  ]
+  providers: [CoursesHttpService, CourseEntityService],
 })
 export class CoursesModule {
-
-  constructor() {
-
+  constructor(private eds: EntityDefinitionService) {
+    eds.registerMetadataMap(entityMetaData);
   }
-
-
 }
